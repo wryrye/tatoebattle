1fbdeb34959d9935158d063cb600a141f2d7d80e
wryrye <ryan.coughs@gmail.com>
2019-09-28 11:33:08 -0400

    flush redis before pushing keys

--- python/redis-ingest.py
+++ python/redis-ingest.py
@@ -2,19 +2,15 @@ import os
 import redis
 import pandas as pd
 
-
 r = redis.from_url(os.environ.get("REDIS_URL"))
-# r.set('foo', 'bar')
-# print(r.get('foo'))
+
+r.flushall()
 
 pipe = r.pipeline()
 
 df = pd.read_csv("both.csv", names = ["id", "eng", "simp", "trad"])
-# r.delete('1')
 
 for index, row in df.iloc[1:].iterrows():
     pipe.lpush(index, *[row['eng'],row['simp'],row['trad']])
 
 pipe.execute()
-
-# print(r.lindex(20,1).decode('utf-8'))
