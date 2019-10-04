b5bdaad179cd50a0d231994845ffcfbd659326ef
wryrye <ryan.coughs@gmail.com>
2019-10-04 01:58:32 -0400

    add python filter script

---
+++ python/prepare/filter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: ryan
+"""
+
+import sys
+import csv
+
+src_dir = '../source'
+lang = sys.argv[1]
+
+with open(f'{src_dir}/sentences.csv','r') as sents, open(f'{src_dir}/links.csv','r') as links:
+    writer = csv.writer(open(f'{lang}.csv', 'w'))
+    for rowi in csv.reader(sents,delimiter='\t'):
+        if rowi[1] == lang:
+            writer.writerow(rowi)
