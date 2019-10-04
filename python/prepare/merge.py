e6d7f3a1109c271ca44deba64286b4d4d83f58bb
wryrye <ryan.coughs@gmail.com>
2019-10-04 04:40:02 -0400

    fix link logic and update translations

--- python/prepare/merge.py
+++ python/prepare/merge.py
@@ -14,7 +14,6 @@ lang2 = sys.argv[2]
 
 merge_file = f'{lang1}_{lang2}_translations.csv'
 
-# WARNING: may cause duplicates
 last_line = None
 if os.path.exists(merge_file):
     with open(merge_file) as f:
@@ -25,17 +24,20 @@ if os.path.exists(merge_file):
 last_id = 0 if last_line is None else int(last_line[0:last_line.index(',')])
 print(f'Last ID: {last_id}') 
 
-# TODO: store as list for multiple versions of translation
 print('Storing links in memory...')
-dict_links = {}
 with open(f'{src_dir}/links.csv','r') as links:
+    dict_links = {}
     for row_links in csv.reader(links, delimiter='\t'):
         id_link = int(row_links[0])
-        dict_links[id_link] = int(row_links[1]);
+
+        if id_link in dict_links:
+            dict_links[id_link].append(int(row_links[1]))
+        else:
+            dict_links[id_link] = [int(row_links[1])];
 
 print('Storing 2nd language in memory...')
-dict_sent2 = {}
 with open(f'{lang2}_sentences.csv','r') as sents2:
+    dict_sent2 = {}
     for row_sents2 in csv.reader(sents2):
         id_sent2 = int(row_sents2[0])
         dict_sent2[id_sent2] = row_sents2;
@@ -50,9 +52,10 @@ with open(f'{lang1}_sentences.csv','r') as sents1:
             continue
 
         if id_sent1 in dict_links:
-            id_target = dict_links[id_sent1]
+            target_list = dict_links[id_sent1]
 
-            if id_target in dict_sent2:
-                merged = row_sents1 + dict_sent2[id_target]
-                print('Match found:', merged);
-                writer.writerow(merged)
+            for id_target in target_list:
+                if id_target in dict_sent2:
+                    merged = row_sents1 + dict_sent2[id_target]
+                    print('Match found:', merged);
+                    writer.writerow(merged)
